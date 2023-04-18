8621c2f71dc82268ec13c88411b6dc812f3ba8cf
Janiece-Lewis <jlewilewi@gmail.com>
2023-04-18 18:32:15 -0400

    added view func.

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ require('console.table');
 require('dotenv').config();
 
 
+// connects to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        // MySQL username,
+
         user: process.env.DB_USER,
-        // TODO: Add MySQL password here
+    
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
     },
-    console.log(`Connected to the movies_db database.`)
+   
 );
 db.connect((err) => {
     if (err) throw err;
@@ -29,36 +30,8 @@ const questions = [
         name: 'answer',
         choices: ['view all departments', "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
     },
-    // {
-    //   type: 'input',
-    //   message: 'view all roles',
-    //   name: 'view-all-roles',
-    // },
-    // {
-    //   type: 'input',
-    //   message: 'view all employees',
-    //   name: 'view-all-employees',
-    // },
-    // {
-    //   type: 'input',
-    //   message: 'add a dept',
-    //   name: 'add-dept',
-    // },
-    // {
-    //   type: 'input',
-    //   message: 'add roles',
-    //   name: 'add-a-role',
-    // },
-    // {
-    //   type: 'input',
-    //   message: 'add an employee',
-    //   name: 'add-employee',
-    // },
-    //  {
-    //   type: 'input',
-    //   message: 'update an employee role',
-    //   name: 'update-employee-role',
-    // },
+
+    
 
 ];
 
@@ -68,24 +41,18 @@ const questions = [
 function init() {
     inquirer.prompt(questions)
         .then((answers) => {
-            console.log(answers.answer)
+            // console.log(answers.answer)
             // switch for answers
             switch (answers.answer) {
                 case "view all departments":
                 viewDept()
                     break;
                 case "view all roles":
-                    console.log(('1, "sales lead", 100000,1'),
-                        ('2, "Salesperson", 80000,1'),
-                        ('3, "Lead Engineer", 150000,2'),
-                        ('4, "Software Engineer", 120000,2'),
-                        ('5, "Account Manager",160000,3'),
-                        ('6, "Accountant", 125000,3'),
-                        ('7, "Legal Team Lead", 250000,4'),
-                        ('8, "Lawyer", 190000, 4'))
+                    viewRoles()
                     break;
+
                 case "view all employees":
-                    console.log('see all employees')
+                    viewEmployees()
                     break;
 
                 case "add a department":
@@ -103,11 +70,9 @@ function init() {
                 case "update an employee role":
                     console.log('role updated')
                     break;
-
-
                 default:
-                  db.end()
-                    break;
+                //   db.end()
+                    quit()
             }
         })
 }
@@ -118,6 +83,26 @@ function viewDept() {
       
         console.table(results);
         init()
+      });
+        
+}
+
+function viewRoles() {
+    db.query('SELECT * FROM role',  (err, results)=> {
+        if (err) throw err;
+      
+        console.table(results);
+        init()
+      });
+        
+}
+
+function viewEmployees() {
+    db.query('SELECT * FROM employee',  (err, results)=> {
+        if (err) throw err;
+      
+        console.table(results);
+       init()
       });
         
 }
